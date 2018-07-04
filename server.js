a87aa85eb75bb73ba157002363d0560536e71f19
Daniel Nguyen <danieltn91@gmail.com>
2018-07-03 20:27:35 -0700

    Initalize project

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+//set port
+var port = process.env.PORT || 6000;
+
+app.use(express.static(__dirname));
+app.get('/', function (req,res){
+	res.render("index.html");
+});
+
+app.listen(port,function(){
+	console.log("App is running at port 6000");
+});
